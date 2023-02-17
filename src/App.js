4b5ebc479a57ee77836935f1ec99abf751498cc1
adam.graham <adam.graham@CA-TOR-WKST6TRW.globalservs.com>
2023-02-17 08:39:39 -0500

    displaying the key to test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import RecipeContainer from './components/RecipeContainer';
 import FooterContainer from './components/FooterContainer';
 import './App.scss';
 
+const API_KEY = process.env.REACT_APP_OPENAI_KEY;
+
 const CocktailForm = () => {
   const [spirits, setSpirits] = useState('');
   const [novel, setNovel] = useState('');
@@ -23,6 +25,7 @@ const CocktailForm = () => {
 
   return (
     <ParentContainer>
+      <p>{API_KEY}</p>
       <HeaderContainer spirits={spirits} setSpirits={setSpirits} novel={novel} setNovel={setNovel} handleSubmit={handleSubmit}></HeaderContainer>
       <RecipeContainer loading={loading} recipe={recipe}></RecipeContainer>
       <FooterContainer></FooterContainer>
